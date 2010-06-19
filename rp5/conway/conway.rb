69e5f3f1bde7ea933de8eae2c034542b33d8fc34
Adam Bachman <adam.bachman@gmail.com>
2010-06-18 21:28:45 -0400

    rp5: updated conway's game of life sketch

--- rp5/conway/conway.rb
+++ rp5/conway/conway.rb
@@ -1,5 +1,6 @@
-HEIGHT = 400
-WIDTH  = 400
+HEIGHT = 1600
+WIDTH  = 1800
+SCALING = 20.0
 
 DXY = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
 # a 400 by 400 2D array
@@ -74,26 +75,104 @@ def patterns
   }
 end
 
-# move a collection of points centered on 0,0 to the middle of the sketch
-def to_center l
-  l.map {|(x, y)| [x + width / 2, y + height / 2]}
+def place_pattern l, x_off, y_off
+  # x_off, y_off = random(width), random(height)
+  l.map {|(x, y)| [x + x_off, y + y_off ]}
+end
+
+def scale p
+  [ p[0] * @scaling, p[1] * @scaling ]
 end
 
 def setup
   size(WIDTH, HEIGHT)
-  background 0
   noStroke
+  smooth
   fill 255
-  frameRate 20
-
-  @world = to_center(patterns[:acorn])
+  frameRate 60
+  background 0
+  #@world = to_center(patterns[:acorn])
+  @world = []
+  @step = 0
+  @scaling = SCALING
+  @alpha = 3
 end
 
 def draw
+  #@scaling = map(mouse_x, 0, width, 0, 20)
+  #@alpha = map(mouse_y, 0, height, 0, 100)
+
   # puts @world.inspect
-  background 0
-  @world.each {|(x, y)|
-    set(x, y, color(255)) if x < width && x > 0 && y < height && y > 0
+  # background 0
+  fill 0, @alpha
+  rect 0, 0, width, height
+  fill 255, 40
+
+  @world = @world.select {|p|
+    x, y = scale p
+    x < width && x > 0 && y < height && y > 0
   }
+
+  # draw points
+  @world.each {|p|
+    x, y = scale p
+    if x < width && x > 0 && y < height && y > 0
+      ellipse(x, y, @scaling * 2, @scaling * 2)
+    end
+  }
+  @step += 1
   @world = step(@world)
+
+  # delete a random spot every 2 steps
+  if @step % 20 == 0
+    @world.pop(random(@world.length))
+  end
+
+  if @step % 3 == 0
+    launch random(width), random(height)
+  end
+
+  if @step % 100 == 0
+    puts 'closer...'
+  end
+
+  if @step > 300
+    save_frame("background.png")
+    exit
+  end
+end
+
+def rotate_pattern p
+  i = random(0, 4).to_i
+  case i
+  when 0
+    p.map {|(x, y)| [-x, y]}
+  when 1
+    p.map {|(x, y)| [x, -y]}
+  when 2
+    p.map {|(x, y)| [-x, -y]}
+  when 3
+    p
+  end
+end
+
+def launch x, y
+  p = rotate_pattern(patterns[:glider])
+  x_off = map(x, 0, width, 0, width / @scaling)
+  y_off = map(y, 0, height, 0, height / @scaling)
+  place_pattern(p, x_off, y_off).inject(@world, &:<<)
+end
+
+def mouse_pressed
+  launch mouse_x, mouse_y
+end
+
+def reset_world
+  @world = []
+end
+
+def key_pressed
+  if key == ' '
+    reset_world
+  end
 end
