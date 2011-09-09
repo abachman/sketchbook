bbb22181556dcdeade55e89366ed9790af8bf845
Adam Bachman <adam.bachman@gmail.com>
2011-09-08 20:41:57 -0400

    updated arcify

--- rp5/arcify.rb
+++ rp5/arcify.rb
@@ -7,7 +7,7 @@ class Arcify < Processing::App
       @x = x
       @y = y
       @r = r
-      @s = s
+      @s = s # this is a thing
       @f = f
       @id = i
       @color = color(map(@r, 0, width, 0, 100), 50, 255)
@@ -34,15 +34,20 @@ class Arcify < Processing::App
     stroke_cap SQUARE
     stroke 255
 
+    @img = load_image("/Users/adam/Desktop/2011-05-16-163758.jpeg")
+
     @s = 0
     @f = PI / 2
     keys = %w(q w e r t y u i o p a s d f g h)
-    @pressed = keys.inject({}) {|a, k| 
+    @pressed = keys.inject({}) {|a, k|
       a[k]=false; a }
 
-    @as = (100..900).step(80).map do |r|
+    @as = (100..900).step(100).map do |r|
       Arc.new 0, 0, r, @s, @f, keys.shift
     end
+
+    @count = 0
+    frame_rate 60
   end
 
   def draw
@@ -50,6 +55,10 @@ class Arcify < Processing::App
     no_stroke
     rect 0, 0, width, height
     no_fill
+
+    # if (@count += 1) % 60 === 0
+    #   image(@img, 0, 0, width, height) # asdf
+    # end
 
     push_matrix
     translate width/2, height/2
@@ -65,6 +74,7 @@ class Arcify < Processing::App
   def key_pressed
     @pressed[key] = !@pressed[key]
   end
+
   # def key_released
   #   @pressed[key] = false
   # end
