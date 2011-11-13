3c2fdc87f116b2d6e61e70683f484008d713abdc
Adam Bachman <adam.bachman@gmail.com>
2011-11-12 20:48:03 -0500

    make it make fire

--- rp5/randomness/perlin.rb
+++ rp5/randomness/perlin.rb
@@ -1,18 +1,30 @@
+def mode
+  :fire
+end
+
 def setup
   size 640, 480
   background 51
-  color_mode HSB
-  noStroke
+  @cell_size = 8
   frameRate 20
 
-  @cell_size = 32
+  if mode == :fire
+    @xy_offset = 0
+  elsif mode == :smoke
+    # pass
+    frameRate 1
+  else
+    color_mode HSB
+  end
 
-  @noise_scale = 0.03
-  @white = color 255
+  noise_seed random(100)
+  noStroke
+
+  @img = load_image 'gradient.png'
+
+  @noise_scale = 0.02
 
   @z_value = 0
-
-  @xy_offset = 0
 end
 
 # def draw
@@ -34,21 +46,57 @@ end
 
 def draw
   background 51
-  step_through_z
+  if mode == :fire
+    make_fire
+  elsif mode == :smoke
+    load_pixels
+    p_count = 0
+    height.times do |y|
+      width.times do |x|
+        nx = (mouseX + x) * @noise_scale
+        ny = (mouseY + y) * @noise_scale
+        # nz = @z_value * @noise_scale
+        pixels[p_count] = color 255 * noise(nx, ny)
+        p_count += 1
+      end
+    end
+    @z_value += 1
+    update_pixels
+    # no_loop
+  else
+    heat_mapper
+  end
 end
 
-def step_through_z
+def heat_mapper
   (width / @cell_size).times do |x|
     (height / @cell_size).times do |y|
-      nx = (x) * @noise_scale
-      ny = (@xy_offset + y) * @noise_scale
+      nx = x * @noise_scale
+      ny = y * @noise_scale
       nz = @z_value * @noise_scale
       fill 255 * noise(nx, ny, nz), 255, 255
       rect x * @cell_size, y * @cell_size, @cell_size, @cell_size
     end
   end
   @z_value += 1
-  @xy_offset += 1
+end
+
+def make_fire
+  (width / @cell_size).times do |x|
+    (height / @cell_size).times do |y|
+      nx = (x) * @noise_scale
+      ny = (@xy_offset + y) * @noise_scale
+      nz = @z_value * @noise_scale
+      fill 255 * noise(nx, ny, nz), 0, 0
+      rect x * @cell_size, y * @cell_size, @cell_size, @cell_size
+    end
+  end
+
+  (width / 16).times do |n|
+    image(@img, n * 16, 0)
+  end
+  @xy_offset += 2
+  @z_value += 1
 end
 
 # def mouse_moved
